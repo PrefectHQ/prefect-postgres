aec004b5729760f1e6810b2d178a9aed04151c74
ahuang11 <andrew.h@prefect.io>
2022-05-02 18:39:24 -0600

    Add contents

--- prefect_postgres/credentials.py
+++ prefect_postgres/credentials.py
@@ -1,1 +1,73 @@
-"""Credential classes used to perform authenticated interactions with prefect_postgres"""
+"""Credentials class to authenticate Postgres."""
+
+from dataclasses import dataclass
+from typing import Optional
+
+import psycopg2
+
+
+@dataclass
+class PostgresCredentials:
+    """
+    Dataclass used to manage authentication with Postgres.
+
+    Args:
+        user: The user name used to authenticate.
+        password: The password used to authenticate.
+        database: The name of the Postgres database.
+        host: The host address of the database.
+        port: The port to connect to the database.
+    """  # noqa
+
+    user: str
+    password: str
+    database: Optional[str] = None
+    host: Optional[str] = "localhost"
+    port: Optional[int] = 5432
+
+    def get_connection(
+        self, database: Optional[str] = None
+    ) -> psycopg2.extensions.connection:
+        """
+        Returns an authenticated connection that can be
+        used to query from Postgres databases.
+
+        Args:
+            database: The name of the database to use; overrides
+                the class definition if provided.
+    
+        Returns:
+            The authenticated Postgres connection.
+
+        Examples:
+            ```python
+            from prefect import flow
+            from prefect_postgres import PostgresCredentials
+
+            @flow
+            def postgres_credentials_flow():
+                postgres_credentials = PostgresCredentials(
+                    user="user",
+                    password="password",
+                    database="postgres",
+                    host="127.0.0.1"
+                )
+                return postgres_credentials
+
+            postgres_credentials_flow()
+            ```
+        """
+        database = self.database or database
+        if database is None:
+            raise ValueError(
+                f"The database must be set in either "
+                f"PostgresCredentials or the task"
+            )
+
+        return psycopg2.connect(
+            database=database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,3 +1,31 @@
+import pytest
 from unittest.mock import MagicMock
 
 from prefect import flow
+
+from prefect_postgres.credentials import PostgresCredentials
+
+
+def test_postgres_credentials_get_connection_override(monkeypatch):
+    connect_mock = MagicMock()
+    monkeypatch.setattr("psycopg2.connect", connect_mock)
+
+    connection = PostgresCredentials(
+        "user",
+        "password",
+        database="database"
+    ).get_connection(
+        database="override_database"
+    )
+    connect_mock.assert_called_with(
+        database='database',
+        user='user',
+        password='password',
+        host='localhost',
+        port=5432
+    )
+
+
+def test_postgres_credentials_get_connection_missing_database():
+    with pytest.raises(ValueError, match=f"The database must be set in either"):
+        PostgresCredentials("user", "password").get_connection()
